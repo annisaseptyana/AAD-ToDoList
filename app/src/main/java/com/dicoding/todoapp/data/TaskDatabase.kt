81f4ba1838326900dad6c7bb10713748944cff4e
annisaseptyana <starrynyteskye@gmail.com>
2022-07-05 12:32:39 +0700

    Implementing database

--- app/src/main/java/com/dicoding/todoapp/data/TaskDatabase.kt
+++ app/src/main/java/com/dicoding/todoapp/data/TaskDatabase.kt
@@ -1,8 +1,10 @@
 package com.dicoding.todoapp.data
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.dicoding.todoapp.R
 import org.json.JSONArray
 import org.json.JSONException
@@ -10,8 +12,14 @@ import org.json.JSONObject
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
+import java.util.concurrent.Executors
 
 //TODO 3 : Define room database class and prepopulate database using JSON
+@Database(
+    entities = [Task::class],
+    version = 1,
+    exportSchema = false
+)
 abstract class TaskDatabase : RoomDatabase() {
 
     abstract fun taskDao(): TaskDao
@@ -21,17 +29,29 @@ abstract class TaskDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: TaskDatabase? = null
 
+        @JvmStatic
         fun getInstance(context: Context): TaskDatabase {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     TaskDatabase::class.java,
                     "task.db"
-                ).build()
+                )
+                    /* .fallbackToDestructiveMigration()
+                    .addCallback(object: Callback() {
+                        override fun onCreate(db: SupportSQLiteDatabase) {
+                            super.onCreate(db)
+                            Executors.newSingleThreadExecutor().execute {
+                                fillWithStartingData(context, getInstance(context).taskDao())
+                            }
+                        }
+                    }) */
+                    .build()
                 INSTANCE = instance
                 instance
             }
         }
+
 
         private fun fillWithStartingData(context: Context, dao: TaskDao) {
             val task = loadJsonArray(context)
